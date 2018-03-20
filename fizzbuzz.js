29ed53589d7e6cd4cffcc3d039a9409572cc385a
Reggie Romano <romanoreggie@gmail.com>
2018-03-20 15:33:24 -0500

    added fizzbuzz logic

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,17 @@
 console.log("Its working");
+
+let a = "Fizz";
+let b = "Buzz";
+
+
+for (i=0; i<=30; i++) {
+	if (i % 15 == 0) {
+		console.log(a+b);
+	} else  if (i % 5 == 0) {
+		console.log(b);
+	} else if (i % 3 == 0) {
+		console.log(a);
+	} else {
+		console.log(i);
+		}
+}
